4885e8ce5d8dc0aacbc00db3402f8aeec2b2517c
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2024-03-26 00:20:21 -0500

    Initial release

---
+++ GarbageNotificationRemoverSystem.cs
@@ -0,0 +1,94 @@
+﻿using Game;
+using Game.Common;
+using Game.Notifications;
+using Game.Prefabs;
+using Game.Tools;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace MagicalGarbageTruck;
+public sealed partial class GarbageNotificationRemoverSystem : GameSystemBase
+{
+    private const int GARBAGE_ICON_INDEX = 10; // there is probably a better way to figure out which icon is the garbage one, but I couldn't find one after trying for a bit
+
+    private EntityQuery _iconPrefabQuery;
+    private EntityQuery _notificationsQuery;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        _iconPrefabQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = [
+                ComponentType.ReadWrite<NotificationIconDisplayData>(),
+            ],
+            None = [
+                ComponentType.ReadOnly<Deleted>(),
+                ComponentType.ReadOnly<Temp>()
+            ]
+        });
+
+        _notificationsQuery = GetEntityQuery(new EntityQueryDesc()
+        {
+            All =
+            [
+                ComponentType.ReadOnly<Icon>(),
+                ComponentType.ReadOnly<PrefabRef>()
+            ],
+            None =
+            [
+                ComponentType.ReadOnly<Deleted>(),
+                ComponentType.ReadOnly<Temp>()
+            ]
+        });
+
+        Mod.log.Info("Injected GarbageNotificationRemoverSystem!");
+        RequireForUpdate(_iconPrefabQuery);
+        RequireForUpdate(_notificationsQuery);
+    }
+
+    protected override void OnUpdate()
+    {
+        var icons = _iconPrefabQuery.ToEntityArray(Allocator.Temp);
+
+        Entity garbageIcon = default;
+        foreach(var icon in icons)
+        {
+            var data = EntityManager.GetComponentData<NotificationIconDisplayData>(icon);
+            if(data.m_IconIndex == GARBAGE_ICON_INDEX)
+            {
+                garbageIcon = icon;
+                break;
+            }
+        }
+
+        if(garbageIcon == default)
+        {
+            Mod.log.Error("No garbage truck problem icon prefab found!");
+            Enabled = false;
+            return;
+        }
+
+        var problems = _notificationsQuery.ToEntityArray(Allocator.Temp);
+#if DEBUG
+        Mod.log.Info($"garbageIcon: {garbageIcon}, problem length: {problems.Length}");
+        var deleted = 0L;
+#endif
+        foreach(var problem in problems)
+        {
+            var prefabRef = EntityManager.GetComponentData<PrefabRef>(problem);
+            if(prefabRef == garbageIcon)
+            {
+                EntityManager.AddComponent<Deleted>(problem);
+#if DEBUG
+                deleted++;
+#endif
+            }
+        }
+
+#if DEBUG
+        Mod.log.Info($"Deleted: {deleted}");
+#endif
+    }
+}
---
+++ MagicalGarbageTruckSystem.cs
@@ -0,0 +1,59 @@
+﻿using Game;
+using Game.Buildings;
+using Game.Common;
+using Game.Tools;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace MagicalGarbageTruck;
+
+public sealed partial class MagicalGarbageTruckSystem : GameSystemBase
+{
+    private EntityQuery _garbageProducerQuery;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        _garbageProducerQuery = GetEntityQuery(new EntityQueryDesc()
+        {
+            All =
+            [
+                ComponentType.ReadWrite<GarbageProducer>()
+            ],
+            None =
+            [
+                ComponentType.ReadOnly<Deleted>(),
+                ComponentType.ReadOnly<Temp>()
+            ]
+        });
+
+        Mod.log.Info("Injected GarbageFixerSystem!");
+        RequireForUpdate(_garbageProducerQuery);
+    }
+
+    protected override void OnUpdate()
+    {
+        var garbageProducers = _garbageProducerQuery.ToEntityArray(Allocator.Temp);
+        foreach (var entity in garbageProducers)
+        {
+            var garbage = EntityManager.GetComponentData<GarbageProducer>(entity);
+            if (garbage.m_Garbage > 0)
+            {
+                var collectionRequest = garbage.m_CollectionRequest;
+
+                garbage.m_Garbage = 0;
+                garbage.m_Flags = GarbageProducerFlags.None;
+                garbage.m_CollectionRequest = default;
+
+                EntityManager.SetComponentData(entity, garbage);
+
+                if (collectionRequest != default)
+                {
+                    EntityManager.AddComponent<Deleted>(collectionRequest);
+                }
+            }
+        }
+    }
+}
+
